d96287c6fac949f8e183cb500171850cf154d7ad
MarkJr <markjr@easydns.com>
2026-05-21 21:03:41 -0400

    Bump tunnelto to 0.1.1

--- Formula/tunnelto.rb
+++ Formula/tunnelto.rb
@@ -1,25 +1,25 @@
 class Tunnelto < Formula
   desc "Public URLs for local AI agents"
   homepage "https://tunnel.to"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.0/tunnelto-darwin-arm64.tar.gz"
-      sha256 "08670f79f7077cb274fab461648930250016266145310129e227a0b7c09fcf2e"
+      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.1/tunnelto-darwin-arm64.tar.gz"
+      sha256 "867fc007ebeff9f2e6fe2a373660d65bbd4c3d2cf0d81419de4778e3f2ca2f11"
     else
-      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.0/tunnelto-darwin-amd64.tar.gz"
-      sha256 "b4b10edd922ad580a1651d4127423bca4679790274e86e72da108c971c110ee3"
+      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.1/tunnelto-darwin-amd64.tar.gz"
+      sha256 "2abd4341d720f59708f10b7ab5bb7f1ab652d65425f7994b70172741a1a50108"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.0/tunnelto-linux-arm64.tar.gz"
-      sha256 "e84bbf5bac5239e175664c7d29d86ac9ed8c0f2e1430c843ea6ebd67bb4e4a29"
+      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.1/tunnelto-linux-arm64.tar.gz"
+      sha256 "2dce0fd9ba3263aa2012d8ad9b80082d21dd756f6be677c7c8fa92b7b559e0e9"
     else
-      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.0/tunnelto-linux-amd64.tar.gz"
-      sha256 "4e18c9fe246c2d8409bb92923114f5975166d2caf5ec29e4a002a55cea4054f8"
+      url "https://github.com/tunnel-to/tunnelto-client/releases/download/v0.1.1/tunnelto-linux-amd64.tar.gz"
+      sha256 "1498132e469ebd97bf6685c72e79585b639ff742a29b9903496d88ffc93c085b"
     end
   end
 
@@ -28,6 +28,6 @@ class Tunnelto < Formula
   end
 
   test do
-    assert_match "tunnelto 0.1.0", shell_output("#{bin}/tunnelto --version")
+    assert_match "tunnelto 0.1.1", shell_output("#{bin}/tunnelto --version")
   end
 end
